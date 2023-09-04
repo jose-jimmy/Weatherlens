9d953d88ca3d69d0ba6a4b0dd277b31d55641f9f
Jose Jimmy <josejimmy8055@gmail.com>
2023-09-04 10:32:04 +0530

    changed from http to https to secure the API call

--- assets/js/api.js
+++ assets/js/api.js
@@ -20,15 +20,15 @@ export const url = {
     return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
   },
   airPollution(lat, lon) {
-    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+    return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
   },
   reverseGeo(lat, lon) {
-    return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+    return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
   },
   /***
    * @param {string} query city name
    */
   geo(query) {
-    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+    return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
   },
 };
